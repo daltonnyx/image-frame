443210bffa1989b7ae3b3edd27ed6ec00ae917b5
daltonnyx <quytruong851@gmail.com>
2017-05-15 04:06:17 +0700

    Thêm nút zoom, thêm nút save

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ var patternSourceCanvas = new fabric.StaticCanvas();
 
 var frame, framepoly, frameimage;
 
-fabric.Image.fromURL('frames/9991083f64f4a3f7d53719b16e5cba68.png', img => {
+fabric.Image.fromURL('frames/saturdy.png', img => {
     frame = img;
     frame.scaleToHeight(canvas.getHeight());
     canvas.setWidth(frame.getWidth());
@@ -43,10 +43,59 @@ reader.addEventListener('load',(file) => {
     }
     fabric.Image.fromURL(reader.result, (img) => {
         frameimage = img;
+        frameimage.scaleToHeight(canvas.getHeight());
+        frameimage.initialScale = frameimage.getScaleY();
+        canvas.remove(canvas._objects[0]);
         canvas.add(frameimage);
         canvas.renderAll();
     });
 },false);
 
+var zoom = document.getElementById('zoom');
 
+zoom.addEventListener('change', zoomChangeHandler, false);
+
+function zoomChangeHandler(evt) {
+    var zoomValue = evt.target.value;
+    var currentObject = canvas._objects[0];
+    currentObject.scale(zoomValue / 100 * currentObject.initialScale);
+    canvas.renderAll();
+}
+
+window.addEventListener('mousewheel', mouseWheelZoomHandler, false);
+window.addEventListener('DOMMouseScroll', mouseWheelZoomHandler, false);
+
+function mouseWheelZoomHandler(evt) {
+    if(evt.target.nodeName != 'CANVAS') 
+        return;
+    if(evt.wheelDelta > 0 || evt.detail > 0) {
+        if(zoom.value < 200) {
+            zoom.value = parseInt(zoom.value) + 10;
+        }
+    }
+    else {
+        if(zoom.value > 10) {
+            zoom.value -= 10;
+        }
+    }
+    var changeEvent = document.createEvent('HTMLEvents');
+    changeEvent.initEvent('change',true,true);
+    zoom.dispatchEvent(changeEvent);
+};
+
+
+var saveButton = document.getElementById('save');
+
+saveButton.addEventListener('click',evt => {
+    evt.preventDefault();
+    var dataURL = canvas.toDataURL({
+        format: 'png'
+    });
+    var downloadAnchor = document.createElement('A');
+    downloadAnchor.download = "image-frame.png";
+    downloadAnchor.href = dataURL;
+    document.body.appendChild(downloadAnchor);
+    downloadAnchor.click();
+    document.body.removeChild(downloadAnchor);
+},false);
 
